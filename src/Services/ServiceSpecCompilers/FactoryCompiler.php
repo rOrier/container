09ad53720036b4c7e8167870aa02a78be6f98bb3
Raphaël Aurières <raphael.aurieres@gmail.com>
2022-04-22 12:24:40 +0200

    Add basic service spec compilers.

---
+++ src/Services/ServiceSpecCompilers/FactoryCompiler.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace ROrier\Services\Services\ServiceSpecCompilers;
+
+use ROrier\Config\Exceptions\ConfigurationException;
+use ROrier\Services\Interfaces\CompilerInterface;
+
+class FactoryCompiler implements CompilerInterface
+{
+    /**
+     * @param array $rawSpecs
+     * @return array
+     * @throws ConfigurationException
+     */
+    public function compile(array $rawSpecs): array
+    {
+        array_walk($rawSpecs, [$this, 'expandFactoryConfiguration']);
+
+        return $rawSpecs;
+    }
+
+    /**
+     * @param array $rawSpec
+     * @param string $name
+     */
+    protected function expandFactoryConfiguration(array &$rawSpec)
+    {
+        if (array_key_exists('factory', $rawSpec)) {
+            $config = $rawSpec['factory'];
+
+            if (!is_array($config)) {
+                $config = array(
+                    'service' => $config
+                );
+            }
+
+            if (!array_key_exists('method', $config)) {
+                $config['method'] = 'build';
+            }
+
+            $rawSpec['factory'] = $config;
+        }
+    }
+}
---
+++ src/Services/ServiceSpecCompilers/InheritanceCompiler.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace ROrier\Services\Services\ServiceSpecCompilers;
+
+use ROrier\Config\Exceptions\ConfigurationException;
+use ROrier\Config\Tools\CollectionTool;
+use ROrier\Services\Interfaces\CompilerInterface;
+
+class InheritanceCompiler implements CompilerInterface
+{
+    private array $rawSpecs;
+
+    /**
+     * @param array $rawSpecs
+     * @return array
+     * @throws ConfigurationException
+     */
+    public function compile(array $rawSpecs): array
+    {
+        $this->rawSpecs = $rawSpecs;
+
+        $specs = [];
+
+        foreach($this->rawSpecs as $name => $definition) {
+            $specs[$name] = $this->compileSpec($name);
+        }
+
+        return $specs;
+    }
+
+    /**
+     * @param string $name
+     * @return array
+     * @throws ConfigurationException
+     */
+    protected function compileSpec(string $name)
+    {
+        if (!array_key_exists($name, $this->rawSpecs)) {
+            throw new ConfigurationException("Unable to retrieve parent service '$name'.");
+        }
+
+        $rawSpec = $this->rawSpecs[$name];
+
+        if (array_key_exists('inherit', $rawSpec)) {
+            $parentRawSpec = $this->compileSpec($rawSpec['inherit']);
+
+            if (array_key_exists('abstract', $parentRawSpec)) {
+                unset($parentRawSpec['abstract']);
+            }
+
+            CollectionTool::merge($parentRawSpec, $rawSpec);
+
+            $rawSpec = $parentRawSpec;
+        }
+
+        return $rawSpec;
+    }
+}
