b7f82acade2bd61431ad832d7885b1b1ca805edf
Raphaël Aurières <raphael.aurieres@gmail.com>
2022-04-22 11:59:06 +0200

    Create Container interfaces.

---
+++ src/Exceptions/ContainerException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ROrier\Services\Exceptions;
+
+use Exception;
+use Psr\Container\ContainerExceptionInterface;
+
+/**
+ * Class ContainerException
+ */
+class ContainerException extends Exception implements ContainerExceptionInterface
+{
+}
---
+++ src/Interfaces/ContainerInterface.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace ROrier\Services\Interfaces;
+
+use Psr\Container\ContainerInterface as PSRContainerInterface;
+use ROrier\Services\Exceptions\ContainerException;
+
+/**
+ * Interface ParametersInterface
+ */
+interface ContainerInterface extends PSRContainerInterface
+{
+    /**
+     * Return TRUE if entry is already instantiated.
+     * @param string $id Identifier of the entry to look for.
+     * @return bool
+     */
+    public function exists(string $id);
+
+    /**
+     * @param string $id
+     * @param object $service
+     * @return self
+     * @throws ContainerException
+     */
+    public function setService(string $id, object $service): ContainerInterface;
+
+    /**
+     * @param object[] $services
+     * @return self
+     * @throws ContainerException
+     */
+    public function setServices(array $services): ContainerInterface;
+
+    /**
+     * @return self
+     */
+    public function reset(): ContainerInterface;
+}
