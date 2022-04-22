a6d281b47b591d081cb8070f01e32623637db4bd
Raphaël Aurières <raphael.aurieres@gmail.com>
2022-04-22 14:01:46 +0200

    Create config parser for service references.

---
+++ src/Services/ConfigParsers/ServiceParser.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace ROrier\Services\Services\ConfigParsers;
+
+use ROrier\Config\Interfaces\ParserInterface;
+use ROrier\Services\Interfaces\ContainerInterface;
+
+class ServiceParser implements ParserInterface
+{
+    private ContainerInterface $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function match(string $var): bool
+    {
+        return (substr($var, 0, 1) === '@');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function process(string $var)
+    {
+        $serviceName = substr($var, 1);
+        $var = $this->container->get($serviceName);
+
+        return $var;
+    }
+}
