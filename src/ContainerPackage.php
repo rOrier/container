f8c815b6b680c6f80b72f974de3ee94c3222be4f
Raphaël Aurières <raphael.aurieres@gmail.com>
2022-04-25 16:52:58 +0200

    Convert config in Yaml format.

--- src/ContainerPackage.php
+++ src/ContainerPackage.php
@@ -6,5 +6,4 @@ use ROrier\Core\Foundations\AbstractPackage;
 
 class ContainerPackage extends AbstractPackage
 {
-    protected const CONFIG_LOADER = 'ROrier\Core\Components\ConfigLoaders\JsonLoader';
 }
